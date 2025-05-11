99652b43211eef4db19a0ded3c9fefb503a16dd3
Richky <richky@student.sgu.ac.id>
2025-05-11 12:59:17 +0700

    update

---
+++ plc/my_nodes.py
@@ -0,0 +1,102 @@
+from asyncua import Client
+import asyncio
+
+url = "opc.tcp://192.168.0.1:4840"
+io_addresses = {
+    # INPUTS
+    "I00": "ns=4;i=16",
+    "I01": "ns=4;i=17",
+    "I02": "ns=4;i=18",
+    "I03": "ns=4;i=19",
+    "I04": "ns=4;i=20",
+    "I05": "ns=4;i=21",
+    "I06": "ns=4;i=22",
+    "I07": "ns=4;i=23",
+    "I10": "ns=4;i=24",
+    "I11": "ns=4;i=25",
+    "I12": "ns=4;i=26",
+    "I13": "ns=4;i=27",
+    "I14": "ns=4;i=28",
+    "I15": "ns=4;i=29",
+
+    # OUTPUTS
+    "Q00": "ns=4;i=5",
+    "Q01": "ns=4;i=6",
+    "Q02": "ns=4;i=7",
+    "Q03": "ns=4;i=8",
+    "Q04": "ns=4;i=9",
+    "Q05": "ns=4;i=10",
+    "Q06": "ns=4;i=11",
+    "Q07": "ns=4;i=12",
+    "Q10": "ns=4;i=13",
+    "Q11": "ns=4;i=14",
+}
+
+io_state = {
+
+    # INPUTS
+    "I00": False,
+    "I01": False,
+    "I02": False,
+    "I03": False,
+    "I04": False,
+    "I05": False,
+    "I06": False,
+    "I07": False,
+    "I10": False,
+    "I11": False,
+    "I12": False,
+    "I13": False,
+    "I14": False,
+    "I15": False,
+
+    # OUTPUTS
+    "Q00": False,
+    "Q01": False,
+    "Q02": False,
+    "Q03": False,
+    "Q04": False,
+    "Q05": False,
+    "Q06": False,
+    "Q07": False,
+    "Q10": False,
+    "Q11": False,
+}
+
+
+# class SubHandler:
+#     def __init__(self, node_map):
+#         # Reverse mapping: NodeId -> I/O name
+#         self.node_to_name = {str_id: name for name, str_id in node_map.items()}
+
+#     def datachange_notification(self, node, val, data):
+#         io_name = self.node_to_name.get(str(node))
+#         print(f"[DATA CHANGE] {io_name} ({node}): New Value = {val}")
+
+
+# async def main():
+#     async with Client(url=url) as client:
+#         print("Connected to OPC UA server.")
+
+#         # Prepare the handler with a mapping of nodeid to I/O name
+#         handler = SubHandler(io_addresses)
+#         sub = await client.create_subscription(25, handler)
+
+#         # Get actual node objects
+#         nodes = [client.get_node(nid) for nid in io_addresses.values()]
+#         handles = await sub.subscribe_data_change(nodes)
+
+#         print(f"Subscribed to {len(nodes)} nodes. Waiting for data changes...")
+
+#         try:
+#             await asyncio.Event().wait()
+#         except KeyboardInterrupt:
+#             print("Unsubscribing...")
+#         finally:
+#             await sub.unsubscribe(handles)
+#             await sub.delete()
+#             print("Subscription terminated.")
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
--- ui/app.py
+++ ui/app.py
@@ -2,8 +2,7 @@ import asyncio
 import threading
 from tkinter import Tk, ttk, Button, Label, Canvas
 from plc.my_client import PLCClient
-
-# opcua/nodes.py
+from plc.my_nodes import SubHandler, io_addresses, io_state
 
 
 class AsyncioLoopThread(threading.Thread):
@@ -32,66 +31,8 @@ class PLCApp:
 
         self.notebook = ttk.Notebook(root)
         self.notebook.pack(fill="both", expand=True)
-        self.node_addresses = {
-            # INPUTS
-            "I00": "ns=4;i=16",
-            "I01": "ns=4;i=17",
-            "I02": "ns=4;i=18",
-            "I03": "ns=4;i=19",
-            "I04": "ns=4;i=20",
-            "I05": "ns=4;i=21",
-            "I06": "ns=4;i=22",
-            "I07": "ns=4;i=23",
-            "I10": "ns=4;i=24",
-            "I11": "ns=4;i=25",
-            "I12": "ns=4;i=26",
-            "I13": "ns=4;i=27",
-            "I14": "ns=4;i=28",
-            "I15": "ns=4;i=29",
-
-            # OUTPUTS
-            "Q00": "ns=4;i=5",
-            "Q01": "ns=4;i=6",
-            "Q02": "ns=4;i=7",
-            "Q03": "ns=4;i=8",
-            "Q04": "ns=4;i=9",
-            "Q05": "ns=4;i=10",
-            "Q06": "ns=4;i=11",
-            "Q07": "ns=4;i=12",
-            "Q10": "ns=4;i=13",
-            "Q11": "ns=4;i=14",
-
-            # MEMORY
-        }
-        self.io_states = {
-            # INPUTS
-            "I00": False,
-            "I01": False,
-            "I02": False,
-            "I03": False,
-            "I04": False,
-            "I05": False,
-            "I06": False,
-            "I07": False,
-            "I10": False,
-            "I11": False,
-            "I12": False,
-            "I13": False,
-            "I14": False,
-            "I15": False,
-
-            # OUTPUTS
-            "Q00": False,
-            "Q01": False,
-            "Q02": False,
-            "Q03": False,
-            "Q04": False,
-            "Q05": False,
-            "Q06": False,
-            "Q07": False,
-            "Q10": False,
-            "Q11": False,
-        }
+        self.node_addresses = io_addresses
+        self.io_states = io_state
 
         self.create_tabs()
 
